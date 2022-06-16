3e4216bc162f05be6151a95231e4582849190210
Megan <iamdun24@gmail.com>
2022-06-16 12:13:36 -0400

    update

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import Todo from './Todo'
 import TodoForm from './TodoForm'
 
 function TodoList() {
@@ -12,12 +13,23 @@ function TodoList() {
     const newTodos = [todo, ...todos]
 
     setTodos(newTodos)
+    };
+
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+        if (todo.id ==== id) {
+            todo.isComplete =!todo.isComplete
+        }
+        return todo
+        })
+
     }
 
   return (
     <div>
         <h1>What's the Plan for Today?</h1>
         <TodoForm onSubmit={addTodo} />
+        <Todo todos={todos} completeTodo={completeTodo} />
     </div>
   )
 }
